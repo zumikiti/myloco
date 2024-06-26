c7e99604c1349f204b44fb69fb4405b6c9ffa7bd
akihiro <aponmaru@gmail.com>
2024-06-26 23:08:20 +0900

    add: articles model, migration

--- src/models/_entities/articles.rs
+++ src/models/_entities/articles.rs
@@ -4,13 +4,14 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
-#[sea_orm(table_name = "notes")]
+#[sea_orm(table_name = "articles")]
 pub struct Model {
     pub created_at: DateTime,
     pub updated_at: DateTime,
     #[sea_orm(primary_key)]
     pub id: i32,
     pub title: Option<String>,
+    #[sea_orm(column_type = "Text", nullable)]
     pub content: Option<String>,
 }
 
