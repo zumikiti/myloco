e2d693cdaa312b87b99f53fbcf8745b9604f51a6
akihiro <aponmaru@gmail.com>
2024-06-28 19:26:08 +0900

    add: articles の CRUD API 実装

--- src/controllers/articles.rs
+++ src/controllers/articles.rs
@@ -1,15 +1,67 @@
 #![allow(clippy::unused_async)]
 use loco_rs::prelude::*;
+use serde::{Deserialize, Serialize};
 
-use crate::models::_entities::articles;
+use crate::models::_entities::articles::{ActiveModel, Entity, Model};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Params {
+    pub title: Option<String>,
+    pub content: Option<String>,
+}
+
+impl Params {
+    fn update(&self, item: &mut ActiveModel) {
+        item.title = Set(self.title.clone());
+        item.content = Set(self.content.clone());
+    }
+}
+
+async fn load_item(ctx: &AppContext, id: i32) -> Result<Model> {
+    let item = Entity::find_by_id(id).one(&ctx.db).await?;
+    item.ok_or_else(|| Error::NotFound)
+}
 
 pub async fn list(State(ctx): State<AppContext>) -> Result<Response> {
-    let res = articles::Entity::find().all(&ctx.db).await?;
-    format::json(res)
+    format::json(Entity::find().all(&ctx.db).await?)
+}
+
+pub async fn add(State(ctx): State<AppContext>, Json(params): Json<Params>) -> Result<Response> {
+    let mut item = ActiveModel {
+        ..Default::default()
+    };
+    params.update(&mut item);
+    let item = item.insert(&ctx.db).await?;
+    format::json(item)
+}
+
+pub async fn update(
+    Path(id): Path<i32>,
+    State(ctx): State<AppContext>,
+    Json(params): Json<Params>,
+) -> Result<Response> {
+    let item = load_item(&ctx, id).await?;
+    let mut item = item.into_active_model();
+    params.update(&mut item);
+    let item = item.update(&ctx.db).await?;
+    format::json(item)
+}
+
+pub async fn remove(Path(id): Path<i32>, State(ctx): State<AppContext>) -> Result<Response> {
+    load_item(&ctx, id).await?.delete(&ctx.db).await?;
+    format::empty()
+}
+
+pub async fn get_one(Path(id): Path<i32>, State(ctx): State<AppContext>) -> Result<Response> {
+    format::json(load_item(&ctx, id).await?)
 }
 
 pub fn routes() -> Routes {
     Routes::new()
         .prefix("articles")
         .add("/", get(list))
+        .add("/", post(add))
+        .add("/:id", get(get_one))
+        .add("/:id", delete(remove))
+        .add("/:id", post(update))
 }
